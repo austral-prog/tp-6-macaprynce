89f5e58a64321617bf592f0529db414cf7e610e6
Macarena Capra Prince <162723190+macaprynce@users.noreply.github.com>
2024-05-16 22:38:28 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,23 +1,37 @@
 # Replace the "ANSWER HERE" with your answer
     
-lista= input("ingrese una lista:")
-    remove_element = [lista]
-    def remove_elements[0] [4] [5]:
-    return remove_elements
-    print(remove_elements)   
+    def remove_elements(txt):
+        if len(txt) == 0
+            return txt
+        else:
+            del txt[0]
+
+            if len(txt) >= 5:
+                del txt[3:5]
+                return txt
+            elif len(txt) ==4
+                del txt[3]
+                return txt
+            else:
+                return txt
+
+def add_elements(add1):
+        add1.insert(0, "pink")
+        add1.append("yellow")
+        return add1
+
+def is_empty(check):
+    return len(check)== 0
 
 
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def check_lists(compare1, compare2):
+    if len(compare1) >= 3 and len(compare2) >=3:
+        return (compare1[2] == compare2[2])
+    else:
+        return False
 
-
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+def list_of_lists(str):
+    tx1 = str[0][0:2]
+    tx2 = str[1][1:4]
+    tx3 = str[2][-2:]
+    return [tx1, tx2, tx3]
